6d3d0683028450ab42050a7c336a88bf8e75e8e5
Sergey Vagaytsev <thevagser@gmail.com>
2022-11-03 23:36:36 +0700

    Добавил стили + добавление нового item

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,8 @@
 import Item from "./Item"
+import { useState } from "react"
 import '../styles/Board.css'
+import { addItem } from "../store/boardsSlice"
+import { useAppDispatch } from "../hook"
 
 interface IItem {
     id: number
@@ -13,10 +16,26 @@ interface IBoard {
 }
 
 export default function Board(props: IBoard) {
+    const [newItem, setNewItem] = useState('')
+    const dispatch = useAppDispatch()
+
     return(
         <div className="board">
             <h3 className="board__name">{props.title}</h3>
             {props.items.map(item => <Item key={item.id} id={item.id} title={item.title}/>)}
+            <div className="newItem">
+                <input 
+                    type="text"
+                    value={newItem}
+                    onChange={e => setNewItem(e.target.value)}
+                />
+                <button
+                    onClick={e => {
+                        dispatch(addItem({boardID: props.id, newItem}))
+                        setNewItem('')
+                    }}
+                >+</button>
+            </div>
         </div>
     )
 }
--- src/store/boardsSlice.ts
+++ src/store/boardsSlice.ts
@@ -15,6 +15,11 @@ type BoardsState = {
     boards: Board[]
 }
 
+type NewItem = {
+    boardID: number
+    newItem: string
+}
+
 const initialState: BoardsState = {
     boards: [
         {id: 1, title: 'Надо сделать', items: [{id: 1, title: 'Прийти домой'}, {id: 2, title: 'Съездить с женой в KFC'}]},
@@ -35,10 +40,13 @@ const boardsSlice = createSlice({
        },
        removeBoard(state, action: PayloadAction<number>) {
            state.boards = state.boards.filter(board => board.id !== action.payload)
+       },
+       addItem(state, action: PayloadAction<NewItem>) {
+           state.boards = state.boards.map(board => board.id === action.payload.boardID ? {...board, items: [...board.items, {id: Date.now(), title: action.payload.newItem}]} : board)
        }
     }
 })
 
-export const {addBoard, removeBoard} = boardsSlice.actions
+export const {addBoard, removeBoard, addItem} = boardsSlice.actions
 
 export default boardsSlice.reducer
